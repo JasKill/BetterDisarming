058cb32c3cf3eba3c0d93d8f7cc59a04777ffe3d
Todd <infernogaming621@gmail.com>
2018-09-30 15:18:20 -0400

    Efficiency changes

--- BetterDisarming/EscapeHandler.cs
+++ BetterDisarming/EscapeHandler.cs
@@ -9,17 +9,23 @@ namespace BetterDisarming
 	{
 		public EscapeHandler(RoundEventHandler ev, Plugin plugin)
 		{
+			bool ntf = plugin.GetConfigBool("bd_change_ntf_escape");
+			bool ci = plugin.GetConfigBool("bd_change_ci_escape");
+
 			while (ev.roundStarted)
 			{
 				foreach (Player player in plugin.pluginManager.Server.GetPlayers())
 				{
 					Vector pos = player.GetPosition();
-					if ((pos.x >= 174 && pos.x <= 183) && (pos.y >= 980 && pos.y <= 990) && (pos.z >= 25 && pos.z <= 34))
+					if (player.TeamRole.Team.Equals(Team.NINETAILFOX) || player.TeamRole.Team.Equals(Team.CHAOS_INSURGENCY))
 					{
-						if (player.TeamRole.Team.Equals(Team.NINETAILFOX) && player.IsHandcuffed() && plugin.GetConfigBool("bd_change_ntf_escape"))
-							player.ChangeRole(Role.CHAOS_INSUGENCY);
-						else if (player.TeamRole.Team.Equals(Team.CHAOS_INSURGENCY) && player.IsHandcuffed() && plugin.GetConfigBool("bd_change_ci_escape"))
-							player.ChangeRole(Role.NTF_LIEUTENANT);
+						if ((pos.x >= 174 && pos.x <= 183) && (pos.y >= 980 && pos.y <= 990) && (pos.z >= 25 && pos.z <= 34))
+						{
+							if (player.TeamRole.Team.Equals(Team.NINETAILFOX) && player.IsHandcuffed() && ntf)
+								player.ChangeRole(Role.CHAOS_INSUGENCY);
+							else if (player.TeamRole.Team.Equals(Team.CHAOS_INSURGENCY) && player.IsHandcuffed() && ci)
+								player.ChangeRole(Role.NTF_LIEUTENANT);
+						}
 					}
 				}
 				System.Threading.Thread.Sleep(1000);
