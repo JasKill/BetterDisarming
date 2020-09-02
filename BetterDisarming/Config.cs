dc97bcd8d8a15f3a7f8da4e07b30a3ef21c3bc1d
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-02 22:29:14 +0200

    Update Config.cs

--- BetterDisarming/Config.cs
+++ BetterDisarming/Config.cs
@@ -1,4 +1,4 @@
-﻿using Exiled.API.Interfaces;
+using Exiled.API.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel;
 
@@ -12,5 +12,7 @@ namespace BetterDisarming
         public float CheckInterval { get; set; } = 1f;
         [Description("A list of roles who can escape and what they turn into. Format EntryTeam:ExitTeam")]
         public Dictionary<int, int> Escapelist { get; set; } = new Dictionary<int, int>() { { 15, 8 }, { 13, 8 }, { 4, 8 }, { 11, 8 }, { 12, 8 }, { 8, 13 } };
+        [Description("How many respawn tickets to give to the disarmed person's new team when they escape while cuffed.")]
+        public int GiveTickets { get; set; } = 1;
     }
 }
