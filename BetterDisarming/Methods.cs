2ac807881ee504da397624b8b847bf9520fb26f5
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-02 22:28:56 +0200

    Update Methods.cs

--- BetterDisarming/Methods.cs
+++ BetterDisarming/Methods.cs
@@ -1,16 +1,16 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Collections.Generic;
 using MEC;
 using Exiled.API.Features;
 using UnityEngine;
 using BetterDisarming.Handlers;
+using Respawning;
 
 namespace BetterDisarming
 {
 	public class Methods
 	{
 		private static Vector3 espaceArea = new Vector3(177.5f, 985.0f, 29.0f);
-
 		public static IEnumerator<float> CheckEscape()
 		{
 			while (ServerHandlers.isRoundStarted)
@@ -20,6 +20,12 @@ namespace BetterDisarming
 					if (Plugin.Singleton.Config.Escapelist.ContainsKey((int)player.Role) && Vector3.Distance(espaceArea, player.ReferenceHub.transform.position) <= Escape.radius)
 					{
 						player.Role = (RoleType)Plugin.Singleton.Config.Escapelist[(int)player.Role];
+						if (Plugin.Singleton.Config.GiveTickets > 0)
+						{
+							var team = SpawnableTeamType.NineTailedFox;
+							if (player.Role == RoleType.ClassD || player.Role == RoleType.ChaosInsurgency) team = SpawnableTeamType.ChaosInsurgency;
+							Respawning.RespawnTickets.Singleton.GrantTickets(team, Plugin.Singleton.Config.GiveTickets, false);
+						}
 					}
 				}
 				yield return Timing.WaitForSeconds(Plugin.Singleton.Config.CheckInterval);
