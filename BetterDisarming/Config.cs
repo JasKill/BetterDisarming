af05a4f26d9db5df978ef2b24b7cf080c687ec06
Aevann1 <59999695+Aevann1@users.noreply.github.com>
2020-09-04 15:37:54 +0200

    Add files via upload

--- BetterDisarming/Config.cs
+++ BetterDisarming/Config.cs
@@ -1,4 +1,4 @@
-using Exiled.API.Interfaces;
+﻿using Exiled.API.Interfaces;
 using System.Collections.Generic;
 using System.ComponentModel;
 
@@ -6,8 +6,8 @@ namespace BetterDisarming
 {
     public class Config : IConfig
     {
-        [Description("Wether or not the plugin is enabled.")]
-        public bool IsEnabled { get; set; } = false;
+        [Description("Whether or not the plugin is enabled.")]
+        public bool IsEnabled { get; set; } = true;
         [Description("How long before every escape check in seconds.")]
         public float CheckInterval { get; set; } = 1f;
         [Description("A list of roles who can escape and what they turn into. Format EntryTeam:ExitTeam")]
